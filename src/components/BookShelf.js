17f8604f07956c6da83b024416b6da77b100956a
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-03-01 00:09:21 -0500

    feat: BookShelf component

---
+++ src/components/BookShelf.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+
+class BookShelf extends Component {
+
+  render() {
+    const { books } = this.props
+    const { shelfTitle } = this.props
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover"
+                      style={{
+                        width: 128, height: 193,
+                        backgroundImage: `url(${book.previewLink})`
+                      }}></div>
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">
+                    {book.authors.map(author => <p>{author}</p>)}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default BookShelf
