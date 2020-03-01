e07a71cf0066fc0b784bfb4162ca9ead7262cdf7
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-03-01 17:15:29 -0500

    fix: BookShelf bugs

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -6,19 +6,20 @@ class BookShelf extends Component {
   render() {
     const { books } = this.props
     const { shelfTitle } = this.props
+        
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{shelfTitle}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {books.map((book) => (
-              <li>
+            {books && books.map((book) => (
+              <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover"
                       style={{
                         width: 128, height: 193,
-                        backgroundImage: `url(${book.previewLink})`
+                        backgroundImage: `url(${book.imageLinks.smallThumbnail})`
                       }}></div>
                     <div className="book-shelf-changer">
                       <select>
@@ -32,7 +33,7 @@ class BookShelf extends Component {
                   </div>
                   <div className="book-title">{book.title}</div>
                   <div className="book-authors">
-                    {book.authors.map(author => <p>{author}</p>)}
+                    {book.authors && book.authors.join(', ')}
                   </div>
                 </div>
               </li>
