e2eeb39d2518c19fbb2f12aa6a4f02eb04ffe585
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-03-01 17:17:36 -0500

    feat: Add SearchBook component functionality

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import {search} from '../BooksAPI'
+import serializeForm from 'form-serialize'
+import BookShelf from './BookShelf'
 
 class SearchBook extends Component {
-    handleSubmit = (e) => {
+
+    state = {
+        books: this.props.books
+    }
+    searchBook = (query) => {
+        search(query)
+            .then((result) => {
+                this.setState((prevState) => ({
+                    books: result
+                }))
+            })
+    }
+
+    handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(e.target);
-        // call searchBook function that should come with props
+        const { query } = serializeForm(e.target, { hash: true })
+        this.searchBook(query)
     }
 
     render() {
@@ -15,14 +31,26 @@ class SearchBook extends Component {
                     <Link className='close-search' to="/">close</Link>
                     <div className="search-books-input-wrapper">
                         <form onSubmit={this.handleSubmit}>
-                            <input type="text" placeholder="Search by title or author" />
+                            <input type="text" name="query"
+                                placeholder="Search by title or author" />
+                        <button className='submit-search'
+                            type='submit'
+                            onClick={this.handleSubmit}
+                        >GO</button>
                         </form>
                     </div>
                 </div>
 
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+
+                    </ol>
                 </div>
+                <BookShelf
+                    // Need to pass array of books coming from API 
+                    books={this.state.books}
+                    shelfTitle=''
+                />
             </div>
         )
     }
