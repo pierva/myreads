18b4522d7a131fcde0a7c2876da43b28005e0980
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2020-03-03 20:50:40 -0500

    refactor: Change SearchBook to stateless component

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,66 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import {search} from '../BooksAPI'
 import serializeForm from 'form-serialize'
 import BookShelf from './BookShelf'
 
-class SearchBook extends Component {
 
-    state = {
-        books: this.props.books
-    }
-    searchBook = async (query) => {
-        if (query && query.trim() !== "") {
-            return search(query)
-                .then((result) => {
-                    this.setState((prevState) => ({
-                        books: result
-                    }))
-                })
-        }
-        return this.setState(() => ({
-            books: []
-        }))
-    }
 
-    handleSubmit = async (e) => {
+function SearchBook(props) {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         const form = document.querySelector('#searchBookForm')
         const { query } = serializeForm(form, { hash: true })
-        this.searchBook(query)
+        props.onSearchBook(query)
     }
-
-    render() {
-        return (
-            <div>
-                <div className="search-books-bar">
-                    <Link className='close-search' to="/">close</Link>
-                    <div className="search-books-input-wrapper">
-                        <form id="searchBookForm" onSubmit={this.handleSubmit}>
-                            <input type="text" name="query" required
-                                placeholder="Search by title or author" />
+    return (
+        <div>
+            <div className="search-books-bar">
+                <Link className='close-search' to="/">close</Link>
+                <div className="search-books-input-wrapper">
+                    <form id="searchBookForm" onSubmit={handleSubmit}>
+                        <input type="text" name="query" required
+                            placeholder="Search by title or author" />
                         <button className='submit-search'
                             type='submit'
                             form="searchBookForm"
-                            onClick={this.handleSubmit}
+                            onClick={handleSubmit}
                         >Submit</button>
-                        </form>
-                    </div>
+                    </form>
                 </div>
-
-                <div className="search-books-results">
-                    <ol className="books-grid">
-
-                    </ol>
-                </div>
-                <BookShelf
-                    books={this.state.books}
-                    shelfTitle=''
-                    handleChange={this.props.handleChange}
-                />
             </div>
-        )
-    }
+
+            <div className="search-books-results">
+                <ol className="books-grid">
+
+                </ol>
+            </div>
+            <BookShelf
+                books={props.books}
+                shelfTitle=''
+                handleChange={props.handleChange}
+            />
+        </div>
+    )
 }
 
 export default SearchBook
